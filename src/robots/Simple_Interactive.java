4fefced345fdb9e49818f4c57c1408ee58160830
Will <will.green98@hotmail.com>
2018-04-19 14:00:49 +0100

    Boilerplate for an interactive Tank

---
+++ src/robots/Simple_Interactive.java
@@ -0,0 +1,69 @@
+package robots;
+
+/**
+ * @author Will
+ * This is a simple interactive RoboCode Tank :) 
+ */
+
+import static java.awt.event.KeyEvent.*;
+import java.awt.event.KeyEvent;
+/**
+ * A very simple interactive bot
+ */
+import robocode.AdvancedRobot;
+
+public class Simple_Interactive extends AdvancedRobot {
+	int moveDirection;
+	int turnDirection;
+	double moveAmount;
+	
+	public void run() {
+		while(true) {
+			setAhead(moveAmount * moveDirection);
+			moveAmount = Math.max(0, moveAmount - 1);
+		}
+	}
+	
+	public void onKeyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case VK_UP:
+		case VK_W:
+			moveDirection = 1;
+			moveAmount = Double.POSITIVE_INFINITY;
+			break;
+		case VK_DOWN:
+		case VK_S:
+			moveDirection = -1;
+			moveAmount = Double.POSITIVE_INFINITY;
+			break;
+
+		case VK_RIGHT:
+		case VK_D:
+			turnDirection = 1;
+			break;
+
+		case VK_LEFT:
+		case VK_A:
+			turnDirection = -1;
+			break;
+		}
+	}
+
+	public void onKeyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case VK_UP:
+		case VK_W:
+		case VK_DOWN:
+		case VK_S:
+			moveDirection = 0;
+			moveAmount = 0;
+			break;
+		case VK_RIGHT:
+		case VK_D:
+		case VK_LEFT:
+		case VK_A:
+			turnDirection = 0;
+			break;
+		}
+	}
+}
